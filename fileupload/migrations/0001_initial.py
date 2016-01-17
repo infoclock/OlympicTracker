2565083f94aa1d0df619a74d917603609a9d3cd5
Alex Palcuie <alex.palcuie@hootsuite.com>
2016-01-17 15:04:04 +0200

    Squash migrations and add last_modified #17

--- fileupload/migrations/0001_initial.py
+++ fileupload/migrations/0001_initial.py
@@ -9,20 +9,21 @@ import fileupload.models
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('demo', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Submission',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('file', models.FileField(upload_to=fileupload.models.user_directory_path)),
                 ('slug', models.SlugField(blank=True)),
-                ('url', models.CharField(max_length=200)),
-                ('problem', models.ForeignKey(to='demo.Problem', null=True)),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True)),
+                ('url', models.CharField(blank=True, max_length=200, null=True)),
+                ('last_modified', models.DateField(auto_now=True, null=True)),
+                ('problem', models.ForeignKey(null=True, to='demo.Problem')),
+                ('user', models.ForeignKey(null=True, to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
