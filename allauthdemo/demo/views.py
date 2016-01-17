1496c65a623fe09752adf4aaae7d44ce04da36fe
Alex Palcuie <alex.palcuie@hootsuite.com>
2016-01-17 22:54:15 +0200

    Make ranking better

--- allauthdemo/demo/views.py
+++ allauthdemo/demo/views.py
@@ -1,3 +1,4 @@
+import math
 from django.shortcuts import render
 from django.views import generic
 
@@ -58,7 +59,8 @@ class RankingView(generic.TemplateView):
             d['csacademy'] = user.score_csacademy / 20
             d['max_points'] = 100 if user.school_year == 1 else 140
             d['total'] = d['homework_points'] + d['codeforces_points'] + d['no_stress'] + d['csacademy']
-            d['nota'] = float(d['total']) / float(d['max_points']) * 10
+            pure_grade = math.round(float(d['total']) / float(d['max_points']) * 10)
+            d['nota'] = min(pure_grade, 10)
             users.append(d)
         context['users'] = sorted(users, key=lambda x: x['nota'], reverse=True)
         return context
