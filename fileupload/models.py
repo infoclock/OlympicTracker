97f587bd27f5b86474aa34701140b17fc766e954
Alex Palcuie <alex.palcuie@hootsuite.com>
2016-01-17 13:13:45 +0200

    Put uploaded files in the right folder #12

--- fileupload/models.py
+++ fileupload/models.py
@@ -3,20 +3,22 @@ from django.db import models
 from allauthdemo.auth.models import DemoUser
 from allauthdemo.demo.models import Problem
 
+
 def user_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    # file will be uploaded to MEDIA_ROOT/<problem>/<user_id>_<problem>.<extension>
     extension = filename.split('.')
 
     if len(extension) != 0:
         extension = extension[-1]
-    return 'user_{0}/{1}.{2}'.format(instance.user.id, instance.problem.name, extension)
+    return '{1}/{1}_user_{0}.{2}'.format(instance.user.id, instance.problem.name, extension)
+
 
 class Submission(models.Model):
     file = models.FileField(upload_to=user_directory_path)
     slug = models.SlugField(max_length=50, blank=True)
     user = models.ForeignKey(DemoUser, null=True)
     problem = models.ForeignKey(Problem, null=True)
-    
+
     def __unicode__(self):
         return self.file.name
 
@@ -25,5 +27,12 @@ class Submission(models.Model):
         return ('upload-new', )
 
     def save(self, *args, **kwargs):
-        self.slug = self.file.name
+        # delete the file if it already exists
+        try:
+            submissions = Submission.objects.filter(user_id=self.user_id, problem_id=self.problem_id)
+            for submission in submissions:
+                submission.file.delete(save=False)
+        except:
+            pass
+
         super(Submission, self).save(*args, **kwargs)
