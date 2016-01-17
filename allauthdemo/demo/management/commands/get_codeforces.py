ec749b717e85ba79b6221a3aecd6e5c091042636
Alex Palcuie <alex.palcuie@hootsuite.com>
2016-01-17 16:26:41 +0200

    Write code for API request #15

--- allauthdemo/demo/management/commands/get_codeforces.py
+++ allauthdemo/demo/management/commands/get_codeforces.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from allauthdemo.auth.models import DemoUser
@@ -10,29 +11,36 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--user',
             action='store_true',
-            dest='user_id',
+            dest='user_handle',
             default=False,
         )
 
     def handle(self, *args, **options):
         contest_ids = range(592, 616+1)
-        user_ids = []
-        if options['user_id']:
-            user_ids.append(DemoUser.objects.get(pk=options['user_id']))
+        users = []
+        if options['user_handle']:
+            users.append(DemoUser.objects.get(codeforces_handle=options['user_handle']))
         else:
-            user_ids = DemoUser.objects.all()
+            users = DemoUser.objects.all()
 
         for contest in contest_ids:
             contest_json_path = os.path.join(self.CODEFORCES_JSON_DIR, str(contest) + '.json')
             if (not os.path.isfile(contest_json_path)):
                 self.get_codeforces_json(contest)
 
-        for user in user_ids:
+        for user in users:
             self.get_user_results(user)
 
-
     def get_codeforces_json(self, contest_id):
-        contest_json_path = os.path.join(self.CODEFORCES_JSON_DIR, str(contest) + '.json')
+        print("Getting contest ", contest_id)
+        contest_json_path = os.path.join(self.CODEFORCES_JSON_DIR, str(contest_id) + '.json')
 
-    def get_user_results(self, user_id):
+        resp = requests.get('http://codeforces.com/api/contest.standings?contestId=' + str(contest_id) + '&showUnofficial=true')
+        if resp.status_code == 200:
+            with open(contest_json_path, 'wb') as stream:
+                stream.write(resp.content)
+        else:
+            print("Nu a mers.", resp.content)
+
+    def get_user_results(self, user):
         pass
