d4b412f8f609044d4cc7abdf44d7f8ec9bded428
Alex Palcuie <alex.palcuie@hootsuite.com>
2016-01-17 11:41:57 +0200

    Add migration for Problem model #11

---
+++ allauthdemo/demo/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Problem',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('link', models.CharField(max_length=200)),
+                ('name', models.CharField(max_length=200)),
+                ('score', models.PositiveSmallIntegerField(default=0)),
+            ],
+        ),
+    ]
